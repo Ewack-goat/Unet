85a10f94e63a3d33491396bb28a47fcc02d86df2
Ewack-goat <85799648+Ewack-goat@users.noreply.github.com>
2025-03-11 21:52:46 +0800

    Update unet_model.py

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -1,21 +1,16 @@
-""" Full assembly of the parts to form the complete network """
-
 from .unet_parts import *
+from .cbam import CBAM
 
 
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
 
         super(UNet, self).__init__()
-        #调用父类构造函数：
-        #UNet 继承自 nn.Module（PyTorch 的一个基类），使用 super() 可以调用 nn.Module 的构造函数，以确保基类的初始化逻辑得以执行。
-        #这一步是必须的，确保 UNet 类能够正常使用 nn.Module 提供的方法和属性。
+
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
-        #n_channels：输入图像的通道数（例如 RGB 图像为 3）。
-        #n_classes：输出类别的数量。
-        #bilinear：布尔值，指示是否使用双线性插值进行上采样。
+
 
         self.inc = (DoubleConv(n_channels, 64))
         self.down1 = (Down(64, 128))
@@ -23,6 +18,12 @@ class UNet(nn.Module):
         self.down3 = (Down(256, 512))
         factor = 2 if bilinear else 1
         self.down4 = (Down(512, 1024 // factor))
+
+        self.cbam1 = CBAM(128)  
+        self.cbam2 = CBAM(256)  
+        self.cbam3 = CBAM(512)  
+        self.cbam4 = CBAM(1024 // factor)  
+
         self.up1 = (Up(1024, 512 // factor, bilinear))
         self.up2 = (Up(512, 256 // factor, bilinear))
         self.up3 = (Up(256, 128 // factor, bilinear))
@@ -32,9 +33,13 @@ class UNet(nn.Module):
     def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
+        x2 = self.cbam1(x2)  
         x3 = self.down2(x2)
+        x3 = self.cbam2(x3)  
         x4 = self.down3(x3)
+        x4 = self.cbam3(x4)  
         x5 = self.down4(x4)
+        x5 = self.cbam4(x5)  
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
